4504a4610261411482c99281e35090c93361e5f3
pgraczyk <pgraczyk.de@gmail.com>
2026-07-26 18:20:29 +0200

    retries, cron schedule, dbt profile gitignored

--- airflow/dags/pipeline_dag.py
+++ airflow/dags/pipeline_dag.py
@@ -1,12 +1,18 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+from datetime import datetime, timedelta
+
+default_args = {
+    'retries': 2,
+    'retry_delay': timedelta(minutes=1),
+}
 
 with DAG(
     dag_id='de_pipeline',
     start_date=datetime(2024, 1, 1),
-    schedule='@daily',
-    catchup=False
+    schedule='0 6 * * *',
+    catchup=False,
+    default_args=default_args
 ) as dag:
     extract_task = BashOperator(
         task_id='extract_task',
